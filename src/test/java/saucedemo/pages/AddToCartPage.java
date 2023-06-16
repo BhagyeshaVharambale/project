e985377b10da62faf4c0ec5138f8e65ad2f62270
BhagyeshaVharambale <129709976+BhagyeshaVharambale@users.noreply.github.com>
2023-06-16 14:34:45 +0530

    Update AddToCartPage.java

--- src/test/java/saucedemo/pages/AddToCartPage.java
+++ src/test/java/saucedemo/pages/AddToCartPage.java
@@ -6,8 +6,8 @@ import org.openqa.selenium.WebElement;
 
 public class AddToCartPage {
     
-	WebDriver driver ;
-	WebElement Remove ;
+    WebDriver driver ;
+    WebElement Remove ;
     WebElement ContinueShopping ;
     WebElement Checkout ;
    
@@ -16,34 +16,30 @@ public class AddToCartPage {
     	this.driver = driver ;
     }
 
-	public WebElement getRemove() {
-		setRemove(Remove);
-		return Remove;
-	}
+    public WebElement getRemove() {
+	setRemove(Remove);
+	return Remove;
+    }
 
-	public void setRemove(WebElement remove) {
-		Remove = driver.findElement(By.xpath("//button[text() ='Remove']")) ;
-	}
+    public void setRemove(WebElement remove) {
+	Remove = driver.findElement(By.xpath("//button[text() ='Remove']")) ;
+    }
 
-	public WebElement getContinueShopping() {
-		setContinueShopping(ContinueShopping);
-		return ContinueShopping;
-	}
+    public WebElement getContinueShopping() {
+	setContinueShopping(ContinueShopping);
+	return ContinueShopping;
+    }
 
-	public void setContinueShopping(WebElement continueShopping) {
-		ContinueShopping = driver.findElement(By.cssSelector("button#continue-shopping")) ;
-	}
+    public void setContinueShopping(WebElement continueShopping) {
+	ContinueShopping = driver.findElement(By.cssSelector("button#continue-shopping")) ;
+    }
 
-	public WebElement getCheckout() {
-		setCheckout(Checkout);
-		return Checkout;
-	}
+    public WebElement getCheckout() {
+	setCheckout(Checkout);
+	return Checkout;
+    }
 
-	public void setCheckout(WebElement checkout) {
-		Checkout = driver.findElement(By.xpath("//button[text()='Checkout']")) ;
-	}
-	
-	
-	
-    
+    public void setCheckout(WebElement checkout) {
+	Checkout = driver.findElement(By.xpath("//button[text()='Checkout']")) ;
+    }    
 }
